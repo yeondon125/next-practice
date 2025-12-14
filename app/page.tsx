94fd9498b127536de1772a7f23a1ef240bb5badc
yeondon <s25030@gsm.hs.kr>
2025-12-14 22:51:00 +0900

    main 수정

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Main() {
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`);
+      const res = await fetch(`/api/posts`);
       const data = await res.json();
       setData(data);
     };
